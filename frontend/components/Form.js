cc2681cd107e1d99c83212bc0165c83ffb4c6cb7
Marlon Colato <mcolato2104@gmail.com>
2023-10-22 19:45:11 -0400

    trying

--- frontend/components/Form.js
+++ frontend/components/Form.js
@@ -4,8 +4,14 @@ import * as actionCreators from '../state/action-creators'
 
 export function Form(props) {
   const { postQuiz, inputChange, form } = props;
-  const onChange = (newQuestion, newTrueAnswer, newFalseAnswer) => {
-    inputChange(newQuestion, newTrueAnswer,newFalseAnswer)
+  // const onChange = (newQuestion, newTrueAnswer, newFalseAnswer) => {
+  //   inputChange(newQuestion, newTrueAnswer,newFalseAnswer)
+  // }
+  const onChange = evt => {
+    console.log(evt.target.id)
+    console.log(evt.target.value)
+    inputChange(evt.target.value)
+    
   }
   console.log(form)
   const onSubmit = evt => {
@@ -15,9 +21,9 @@ export function Form(props) {
   return (
     <form id="form" onSubmit={onSubmit}>
       <h2>Create New Quiz</h2>
-      <input maxLength={50} onChange={()=>onChange(newQuestion)} id="newQuestion" placeholder="Enter question" />
-      <input maxLength={50} onChange={()=>onChange(newTrueAnswer)} id="newTrueAnswer" placeholder="Enter true answer" />
-      <input maxLength={50} onChange={()=>onChange(newFalseAnswer)} id="newFalseAnswer" placeholder="Enter false answer" />
+      <input maxLength={50} onChange={onChange} id="newQuestion" placeholder="Enter question" />
+      <input maxLength={50} onChange={onChange} id="newTrueAnswer" placeholder="Enter true answer" />
+      <input maxLength={50} onChange={onChange} id="newFalseAnswer" placeholder="Enter false answer" />
       <button id="submitNewQuizBtn">Submit new quiz</button>
     </form>
   )
